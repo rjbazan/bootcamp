55d35c59f0e683b615b74bb68368aabfcc2673f3
rjbazan <rodrigo.bazan@globant.com>
2015-11-09 17:32:44 -0300

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bootcampAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bootcampAppApp
+ */
+angular.module('bootcampAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bootcampAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bootcampAppApp
+ */
+angular.module('bootcampAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
